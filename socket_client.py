1321f8667c2f31fbb658a60ef9bd1ce546d4a446
Kevin Matthews <kmatthews@cyberdata.net>
2017-10-26 16:32:19 -0700

    Extract signal handling
    No reason to wrap the entire functional program in a try statement.
    Instead, rig up a signal handler.

--- socket_client.py
+++ socket_client.py
@@ -3,8 +3,19 @@
 import socket
 import os
 import sys
+import signal
 
 SOCKET_FD = "/tmp/python_unix_sockets_example"
+client = None
+
+def signal_handler(signal, frame):
+    print("Received keyboard interrupt; shutting down.")
+    if client:
+        client.close()
+    print("Exiting.")
+    sys.exit()
+
+signal.signal(signal.SIGINT, signal_handler)
 
 print("Client connecting...")
 if not os.path.exists(SOCKET_FD):
@@ -17,18 +28,14 @@ client.connect(SOCKET_FD)
 print("Ready; press Ctrl-C to quit.")
 print("Sending 'DONE' shuts down both client and server gracefully.")
 while True:
-    try:
-        x = raw_input("> ")
-        if "" == x:
-            continue
-        print("SEND:", x)
-        msg = x.encode('utf-8')
-        client.send(msg)
-        if "DONE" == x:
-            print("'DONE' received. Shutting down.")
-            break
-    except KeyboardInterrupt as k:
-        print("Received keyboard interrupt; shutting down.")
+    x = raw_input("> ")
+    if "" == x:
+        continue
+    print("SEND:", x)
+    msg = x.encode('utf-8')
+    client.send(msg)
+    if "DONE" == x:
+        print("'DONE' received. Shutting down.")
         break
 
 client.close()
